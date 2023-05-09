a4e8e57814f502500373fc6fbbc855b1e678c3bf
padwhen <phattrong.a@gmail.com>
2023-05-09 11:54:15 +0300

    9/5

--- main.js
+++ main.js
@@ -54,3 +54,26 @@ tabs.forEach(tab => {
         tab.classList.add('qualification__active')
     })
 })
+
+const modalViews = document.querySelectorAll('.services__modal')
+const modalBtns = document.querySelectorAll('services__button')
+const modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn,i) => {
+    modalBtn.addEventListener('click',() => {
+        modal(i)
+        console.log('test')
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click',() => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
